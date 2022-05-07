ad5b349ebf9ab19d368c5565f24cd0e18c37d539
1034189723 <1034189723@qq.com>
2022-05-07 16:09:24 +0800

    1

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -37,7 +37,7 @@ namespace WebApplication1.Controllers
         }
         [HttpPost]
         public async Task<string> GetString() {
-            return "";
+            return "222";
         }
     }
 }
